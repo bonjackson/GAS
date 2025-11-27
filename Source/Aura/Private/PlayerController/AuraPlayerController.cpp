3016e00bd8096d59ac6147a6603676aff6332f34
ringo <sq100605@163.com>
2025-11-27 14:20:21 +0800

    Enhanced Input

---
+++ Source/Aura/Private/PlayerController/AuraPlayerController.cpp
@@ -0,0 +1,25 @@
+
+
+
+#include "PlayerController/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "Net/Core/Misc/NetCVars.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates=true;
+}
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+	UEnhancedInputLocalPlayerSubsystem *SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(SubSystem);
+	SubSystem->AddMappingContext(AuraContext,0);
+	bShowMouseCursor=true;
+	DefaultMouseCursor=EMouseCursor::Default;
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
