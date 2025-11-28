14803218ff61b15db8647957eaa411a00c45fd4d
ringo <sq100605@163.com>
2025-11-28 13:39:09 +0800

    Game Mode

--- Source/Aura/Public/PlayerController/AuraPlayerController.h
+++ Source/Aura/Public/PlayerController/AuraPlayerController.h
@@ -10,6 +10,9 @@
  * 
  */
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
@@ -18,8 +21,13 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 private:
 	UPROPERTY(EditAnywhere,Category="Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+	
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	void Move(const FInputActionValue& InputActionValue);
 };
 
